7abfe079d243c9657784ef0a19dc97cac4afac14
Yeasin Arafath <arafath.yeasin1019@gmail.com>
2023-02-27 00:59:32 +0600

    [feat]: Client Side Initial Setup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ComingSoon from "./images/coming-soon.png";
+
+function App() {
+  return (
+    <div>
+      <img className="app_coming-soon" src={ComingSoon} alt="Coming soon!" />
+    </div>
+  );
+}
+
+export default App;
